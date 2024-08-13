285bf36dd2f4c1ed6793c05b7d855617ae7810c3
Juan Roa <hello@juanroa.dev>
2024-08-13 13:31:59 -0500

    fixed issue with some TS imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config';
+import dotenv from "dotenv";
+dotenv.config();
 import express, { Request, Response } from 'express';
 import { filterAndGroupByDate } from './utils/dateTime';
 import { getPhotoperiodFromAPI, getSolarAPI } from './services/api';
